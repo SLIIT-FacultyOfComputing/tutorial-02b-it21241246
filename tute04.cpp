e98725d28dbb3505277f8b7382bd718a4963d011
Bhagya <95372450+it21241246@users.noreply.github.com>
2022-02-21 12:34:06 +0000

    Converting c programs to c++

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,16 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  int res = 1;
+    for (int i = 2; i <= no; i++)
+        res = res * i;
+    return res;
+
+}
+long nCr(int n, int r)
+{
+  return Factorial(n) / (Factorial(r) * Factorial(n - r));
+
+}
